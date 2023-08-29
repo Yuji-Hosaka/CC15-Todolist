96bbbff46131be248607af3322a15f1591768321
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-08-29 16:18:34 +0700

    Insert logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { FaHome } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='todo'>
+    <div className="todo__header">
+<header className='header'>
+    <div className='header__logo'>
+      <FaHome/>
     </div>
+    <div className='header__text'>
+        <h1>TodoList</h1>
+    </div>
+    <div className='header__search'>
+      <input/>
+    </div>
+</header>
+    </div>
+    <div className="todo__sidebar">SideBar</div>
+    <div classNAme="todo__content">TodoContent</div>
+</div>
   );
 }
 
 export default App;
+
+
+
